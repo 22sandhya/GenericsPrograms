0ae868d26c37434d482da12f499e7f6a7f2f13ea
Admin <sandhyabagale10@gmail.com>
2021-10-11 18:27:56 +0530

    Added:Three Float numbers

--- src/main/java/com/bidgelabs/GenericsPgrm/Maximum.java
+++ src/main/java/com/bidgelabs/GenericsPgrm/Maximum.java
@@ -3,14 +3,15 @@ package com.bidgelabs.GenericsPgrm;
 public class Maximum {
 
 
-	public static Integer getmaximum(Integer a, Integer b, Integer c) {
-		Integer max = a;
+	public static Float getmaximum(Float a, Float b, Float c) {
+		Float max = a;
 		if (b.compareTo(max) > 0 && b.compareTo(c) > 0)
 			max = b;
 		else if (c.compareTo(max) > 0)
 			max = c;
-	
+		System.out.println(max);
 		return max;
 
 	}
+	
 }
---
+++ src/test/java/com/bidgelabs/GenericsPgrm/MaximumTest.java
@@ -0,0 +1,25 @@
+package com.bidgelabs.GenericsPgrm;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class MaximumTest {
+
+	@Test
+	public void given3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue1() {
+		float finalResult = Maximum.getmaximum(9.6f, 1.4f, 4.4f);
+		Assert.assertEquals(9.6f, finalResult, 0.0);
+	}
+
+	@Test
+	public void given3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue2() {
+		float finalResult = Maximum.getmaximum(1.4f, 9.6f, 4.4f);
+		Assert.assertEquals(9.6f, finalResult, 0.0);
+	}
+
+	@Test
+	public void given3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue3() {
+		float finalResult = Maximum.getmaximum(9.6f, 1.4f, 4.4f);
+		Assert.assertEquals(9.6f, finalResult, 0.0);
+	}
+}
