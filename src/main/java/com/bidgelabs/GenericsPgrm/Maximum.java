e76401211b09fa772fcd681eed43e19d81535217
Admin <sandhyabagale10@gmail.com>
2021-10-11 18:21:30 +0530

    Added:Three Integers added

---
+++ src/main/java/com/bidgelabs/GenericsPgrm/Maximum.java
@@ -0,0 +1,16 @@
+package com.bidgelabs.GenericsPgrm;
+
+public class Maximum {
+
+
+	public static Integer getmaximum(Integer a, Integer b, Integer c) {
+		Integer max = a;
+		if (b.compareTo(max) > 0 && b.compareTo(c) > 0)
+			max = b;
+		else if (c.compareTo(max) > 0)
+			max = c;
+	
+		return max;
+
+	}
+}
---
+++ src/test/java/com/bidgelabs/GenericsPgrm/MaxNumFirstPosition.java
@@ -0,0 +1,25 @@
+package com.bidgelabs.GenericsPgrm;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class MaxNumFirstPosition {
+	@Test
+	public void checkmaximum1() {
+		int max = Maximum.getmaximum(9, 4, 1);
+		Assert.assertEquals(9, max);
+	}
+
+	@Test
+	public void checkmaximum2() {
+		int max = Maximum.getmaximum(1, 9, 4);
+		Assert.assertEquals(9, max);
+	}
+
+	@Test
+	public void checkmaximum3() {
+		int max = Maximum.getmaximum(1, 4, 9);
+		Assert.assertEquals(9, max);
+	}
+
+}
